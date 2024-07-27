32cd12fcc0e81f2fb1fb86a142b41c5a512bd667
Yorick de Wid <yorickdewid@users.noreply.github.com>
2024-07-27 12:26:24 +0200

    Add motion commands

--- src/services/WebRTC/commands/motion.ts
+++ src/services/WebRTC/commands/motion.ts
@@ -1,48 +1,83 @@
-// import { Command } from "."
-// import type { ICommand } from "."
+import { MessageType, type IMessage } from "."
 
 /******************************************************************************
  * Motion Change
  */
-// export declare type TMotionChangeCommandParams = {
-//   actuator: string
-//   value: number
-// }
+export enum Actuator {
+  BOOM = 0,
+  ARM = 4,
+  ATTACHMENT = 5,
+  SLEW = 1,
+  LIMP_LEFT = 3,
+  LIMP_RIGHT = 2
+}
 
-// export interface IMotionCommand extends ICommand {
-//   topic: 'motion'
-// }
-// export class MotionCommand extends Command implements IMotionCommand {
-//   topic: 'motion' = 'motion'
-// }
+export enum MotionType {
+  STOP_ALL = 0x00,
+  RESUME_ALL = 0x01,
+  RESET_ALL = 0x02,
+  STRAIGHT_DRIVE = 0x05,
+  CHANGE = 0x10
+}
 
-// export interface IMotionChangeCommand extends IMotionCommand {
-//   payload: {
-//     type: number
-//     change?: TMotionChangeCommandParams[]
-//   }
-// }
+export declare type TMotionChangeSet = {
+  actuator: Actuator
+  value: number
+}
 
-// export class MotionChangeCommand extends MotionCommand implements IMotionChangeCommand {
-//   payload: { type: number; change: TMotionChangeCommandParams[] };
+export declare type TMotionStraightDrive = number
 
-//   constructor(type: number, { actuator, value }: TMotionChangeCommandParams) {
-//     super()
+export class Motion implements IMessage {
+  messageType: MessageType = MessageType.MOTION
 
-//     this.payload = {
-//       type,
-//       change: [
-//         { actuator, value }
-//       ]
-//     }
-//   }
-// }
+  type: MotionType
+  straightDrive?: TMotionStraightDrive
+  change?: TMotionChangeSet[]
 
+  constructor(type: MotionType, straightDrive?: TMotionStraightDrive, change?: TMotionChangeSet[]) {
+    this.type = type
+    this.straightDrive = straightDrive
+    this.change = change
+  }
 
-// export class MotionStopAllCommand extends MotionCommand implements IMotionCommand {
-//   payload = {
-//     type: 1
-//   }
-// }
+  static fromBytes(data: ArrayBuffer): Motion {
+    const dataView = new DataView(data)
 
+    const type = dataView.getUint8(0) as MotionType
+    let straightDrive: TMotionStraightDrive | undefined
+    let change: TMotionChangeSet[] | undefined
 
+    if (type === MotionType.STRAIGHT_DRIVE) {
+      straightDrive = dataView.getInt16(1, false)
+    } else if (type === MotionType.CHANGE) {
+      const changeCount = dataView.getUint8(1)
+      change = new Array(changeCount)
+      for (let i = 0; i < changeCount; i++) {
+        change[i] = {
+          actuator: dataView.getUint8(2 + i * 3) as Actuator,
+          value: dataView.getInt16(3 + i * 3, false)
+        }
+      }
+    }
+
+    return new Motion(type, straightDrive, change)
+  }
+
+  toBytes(): ArrayBuffer {
+    const buffer = new ArrayBuffer(2)
+    const dataView = new DataView(buffer)
+
+    dataView.setUint8(0, this.type)
+    if (this.straightDrive) {
+      dataView.setInt16(1, this.straightDrive, false)
+    } else if (this.change) {
+      dataView.setUint8(1, this.change.length)
+      this.change.forEach((change, i) => {
+        dataView.setUint8(2 + i * 3, change.actuator)
+        dataView.setInt16(3 + i * 3, change.value, false)
+      })
+    }
+
+    return buffer
+  }
+}
